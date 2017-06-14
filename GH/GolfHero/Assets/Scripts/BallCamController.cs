639c1d45ecfdf4c6dba5b7ea6c44f0279b4de55e
Swarnajyoti Datta <swarna.datta@mail.utoronto.ca>
2017-06-14 19:10:39 -0400

    Implemented third-person ball cam. X, Y, and zoom sensitivity are moddable.

--- GH/GolfHero/Assets/Scripts/BallCamController.cs
+++ GH/GolfHero/Assets/Scripts/BallCamController.cs
@@ -3,18 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BallCamController : MonoBehaviour {
+    private const float Y_ANGLE_MIN = 0.0f;
+    private const float Y_ANGLE_MAX = 50.0f;
+    private const float DISTANCE_MIN = 2f;
+    private const float DISTANCE_MAX = 16f;
 
-    public GameObject ball;
+    public Transform target;
+    private Transform camTransform;
 
-    private Vector3 offset;
+    private Camera cam;
+
+    private float currentDistance;
+    private float currentX;
+    private float currentY;
+    private float sensitivityX;
+    private float sensitivityY;
+    private float sensitivityZoom;
 
 	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	private void Start () {
+        currentDistance = 9.0f;
+        currentX = 0.0f;
+        currentY = 0.0f;
+        sensitivityX = 1.0f;
+        sensitivityY = 1.0f;
+        sensitivityZoom = 3.0f;
+        camTransform = transform;
+        cam = GetComponent<Camera>();
+    }
+
+    private void Update() {
+        currentX += Input.GetAxis("Mouse X") * sensitivityX;
+        currentY -= Input.GetAxis("Mouse Y") * sensitivityY;
+        currentDistance -= Input.GetAxis("Mouse ScrollWheel") * sensitivityZoom;
+
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+        currentDistance = Mathf.Clamp(currentDistance, DISTANCE_MIN, DISTANCE_MAX);
+    }
+
+    // LateUpdate is called once per frame (
+    void LateUpdate () {
+        Vector3 direction = new Vector3(0, 0, -currentDistance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        camTransform.position = target.position + (rotation * direction);
+        camTransform.LookAt(target.position);
 	}
 }
