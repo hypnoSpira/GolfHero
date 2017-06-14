4db3d9a39bdda293ad5f6f67b5c7de4ac68ff807
Swarnajyoti Datta <swarna.datta@mail.utoronto.ca>
2017-06-14 18:05:56 -0400

    started work on third person camera

---
+++ GH/GolfHero/Assets/Scripts/BallCamController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCamController : MonoBehaviour {
+
+    public GameObject ball;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
