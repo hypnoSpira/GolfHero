7fb4a0906cf238dea876597ebd37ae8f3ab41a3b
Zixing Gong <zixing.gong@mail.utoronto.ca>
2017-06-14 21:25:48 -0400

    [#147168407] implemented ability to hit the ball based on current camera direction

---
+++ GH/GolfHero/Assets/Scripts/HitBallBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBallBehaviour : MonoBehaviour {
+
+    private Camera cam;
+    private Rigidbody rb;
+    private float power = 20;
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 force = cam.transform.forward;
+            Debug.Log(force);
+            rb.AddForce(force * power * power);
+        }
+        //while (Input.GetMouseButtonDown(0)) {
+        //    Mathf.Sin(2);
+        //}
+	}
+}
