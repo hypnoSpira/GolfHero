898e8eb23f617ad19b86fff21db9434073230e8f
John Fraser <johnfras@gmail.com>
2017-06-14 22:14:47 -0400

    Flattened the force of ballHitBehavior

--- GH/GolfHero/Assets/Scripts/HitBallBehaviour.cs
+++ GH/GolfHero/Assets/Scripts/HitBallBehaviour.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HitBallBehaviour : MonoBehaviour {
-
     private Camera cam;
     private Rigidbody rb;
     private float power = 20;
@@ -17,6 +16,11 @@ public class HitBallBehaviour : MonoBehaviour {
 	void FixedUpdate () {
         if (Input.GetMouseButtonDown(0)) {
             Vector3 force = cam.transform.forward;
+            /*Possibly account for ball being on slope */
+            //Vector3 planeNorm = rb.getPlane();
+            Vector3 planeNorm = new Vector3(0,1,0); //Use the norm of the x,z plane
+            //Debug.Log(force);
+            force = Vector3.ProjectOnPlane(force, planeNorm).normalized; //Project onto a flat surface as we dont care about camera height
             Debug.Log(force);
             rb.AddForce(force * power * power);
         }
