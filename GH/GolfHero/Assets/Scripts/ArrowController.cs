0ab96753b836a1cc1adb7cc50de71f1907fbf040
Bryan Delgado-Bosso <bryan.delgado.bosso@gmail.com>
2017-06-14 22:32:24 -0400

    Added arrow to show direction of shot

---
+++ GH/GolfHero/Assets/Scripts/ArrowController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour {
+
+	public Transform arrowTransform;
+	public Transform ballTransform;
+	private Transform camTransform;
+
+	private float camRotationY;
+
+	// Use this for initialization
+	void Start () {
+		camTransform = transform;
+		camRotationY = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		camRotationY = camTransform.eulerAngles.y;
+	}
+
+	void LateUpdate () {
+		Vector3 offset = (arrowTransform.forward * -2);
+		arrowTransform.rotation = Quaternion.Euler (0, camRotationY - 180, 0);
+		arrowTransform.position = ballTransform.position + offset;
+	}
+}
