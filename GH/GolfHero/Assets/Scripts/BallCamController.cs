b57ba63d89e80cac434e1a84610d51c31a3d2c8c
Swarnajyoti Datta <swarna.datta@mail.utoronto.ca>
2017-06-16 19:34:16 -0400

    [Finishes #147312233] Actually fixed it now. Feature moved to BallCamController script, WallHider is now deprecated.

--- GH/GolfHero/Assets/Scripts/BallCamController.cs
+++ GH/GolfHero/Assets/Scripts/BallCamController.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BallCamController : MonoBehaviour {
-    private const float Y_ANGLE_MIN = 0.0f;
+    private const float Y_ANGLE_MIN = -50.0f;
     private const float Y_ANGLE_MAX = 50.0f;
     private const float DISTANCE_MIN = 2f;
     private const float DISTANCE_MAX = 16f;
 
+    // target transform that the camera is pointed at
     public Transform target;
-    private Transform camTransform;
+
+    // enables or disables see-through vision
+    public bool xRayVision;
+
+    private List<Renderer> hiddenRends;
 
     private float currentDistance;
     public float currentHeight;
@@ -27,7 +32,7 @@ public class BallCamController : MonoBehaviour {
         sensitivityX = 1.0f;
         sensitivityY = 1.0f;
         sensitivityZoom = 6.0f;
-        camTransform = transform;
+        hiddenRends = new List<Renderer>();
     }
 
     // Update is called once per frame
@@ -43,12 +48,53 @@ public class BallCamController : MonoBehaviour {
     }
 
     // LateUpdate is called once per frame (at the end of each frame)
-    void LateUpdate () {
+    private void LateUpdate () {
         // Update camera position based on parameters
         Vector3 direction = new Vector3(0, 0, -currentDistance);
         Vector3 height = new Vector3(0, currentHeight, 0);
         Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
-        camTransform.position = target.position + height + (rotation * direction);
-        camTransform.LookAt(target.position);
+        transform.position = target.position + height + (rotation * direction);
+        transform.LookAt(target.position);
+
+        // update hidden objects if applicable
+        if (xRayVision) {
+            updateHiddenObjects();
+        }
 	}
+
+    // Update hidden objects
+    private void updateHiddenObjects()
+    {
+        // raycast from the camera to the target
+        float distance = Vector3.Distance(target.position, transform.position);
+        RaycastHit[] hits = Physics.RaycastAll(transform.position, transform.forward, distance);
+
+        // re-show all previously hidden objects
+        foreach (Renderer rend in hiddenRends)
+        {
+            Color tempColor = rend.material.color;
+            tempColor.a = 1.0f;
+            rend.material.color = tempColor;
+        }
+
+        // hide all objects pierced
+        List<Renderer> updatedRends = new List<Renderer>();
+        for (int i = 0; i < hits.Length; i++)
+        {
+            RaycastHit hit = hits[i];
+            Renderer rend = hit.transform.GetComponent<Renderer>();
+
+            if (rend)
+            {
+                Color tempColor = rend.material.color;
+                tempColor.a = 0.1f;
+                rend.material.color = tempColor;
+                updatedRends.Add(rend);
+            }
+        }
+
+        // book-keep hidden objects for next update
+        hiddenRends = updatedRends;
+    }
+
 }
