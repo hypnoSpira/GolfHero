b5e5eedd67c202bd1164bf4f43ca6a13e8b7def2
Eugenia <eugenia.zhang@mail.utoronto.ca>
2017-06-15 21:32:02 -0400

    [#147306715] Minimap icon/symbol behaviour for 2D sprites; fixing sprites to ignore parent transformation

---
+++ GH/GolfHero/Assets/Scripts/MinimapIconBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapIconBehaviour : MonoBehaviour {
+    private Quaternion fixRotation;
+
+
+	// Use this for initialization
+	void Start () {
+        fixRotation = transform.rotation;
+	}
+
+    // LateUpdate is called once per frame (at the end of each frame)
+    void LateUpdate () {
+        transform.rotation = fixRotation;
+    }
+}
