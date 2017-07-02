81993cb479db342239891aba85fbed5f20e27ac3
Zixing Gong <zixing.gong@mail.utoronto.ca>
2017-07-01 20:08:19 -0400

    [#148189605] Initial implementation of the wind gameplay mechanic

--- GH/GolfHero/Assets/Scripts/HitBallBehaviour.cs
+++ GH/GolfHero/Assets/Scripts/HitBallBehaviour.cs
@@ -2,42 +2,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HitBallBehaviour : MonoBehaviour {
-    
-    private enum ballState {
-        staionary,
-        moving
+
+    private class Position {
+        public Vector3 pos;
+        public Position prev;
+
+        public Position(Vector3 pos, Position prev) {
+            this.pos = pos;
+            this.prev = prev;
+        }
     }
 
+    private Position curr;
     private Camera cam;
     private static Rigidbody rb;
 	public float power;
     public static Vector3 startPos;
-	public GameObject arrow;
+    public Vector3 force;
+    public GameObject arrow;
 	private Renderer[] arrowRend;
+    private int stage = 0;
+    public int[] windSpd = { 0, 0, 0 };
+    public Vector3 windDir;
+    public bool calcWind = true;
+    private Component windTxt;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         cam = Camera.main;
         rb = GetComponent<Rigidbody>();
         startPos = rb.transform.position;
 		arrowRend = arrow.GetComponentsInChildren<Renderer> ();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        if (rb.velocity == new Vector3(0, 0, 0) && Input.GetButton("Fire1")) {
-            Vector3 force = cam.transform.forward;
+        curr = new Position(startPos, null);
+        checkStage();
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (calcWind && rb.velocity == Vector3.zero) {
+            windDir = new Vector3(UnityEngine.Random.Range(-1, 1), 0, UnityEngine.Random.Range(-1, 1));
+            windSpd[2] = UnityEngine.Random.Range(windSpd[0], windSpd[1] + 1);
+            //((Text)windTxt).text = "Wind Speed: " + windSpd[2] +"\nWind Direction:" + windDir;
+            WindText.setText("Wind Speed: " + windSpd[2] + "km/h\nWind Direction: " + windDir);
+            calcWind = false;
+        }
+
+        if (cam.enabled && rb.velocity == Vector3.zero && Input.GetKeyUp("mouse 0")) {
+            cam.enabled = false;
+            force = cam.transform.forward;
+        } else if (!cam.enabled && Input.GetKeyUp("mouse 0")) {
             /*Possibly account for ball being on slope */
             //Vector3 planeNorm = rb.getPlane();
-            Vector3 planeNorm = new Vector3(0,1,0); //Use the norm of the x,z plane
-            //Debug.Log(force);
+            Vector3 planeNorm = new Vector3(0, 1, 0); //Use the norm of the x,z plane
             force = Vector3.ProjectOnPlane(force, planeNorm).normalized; //Project onto a flat surface as we dont care about camera height
             Debug.Log(force);
-            rb.AddForce(force * power * power);
+            rb.AddForce(force * power * power + windDir * windSpd[2] * windSpd[2]);
+            cam.enabled = true;
+            calcWind = true;
+        } else if (Input.GetKeyUp("mouse 1")) {
+            cam.enabled = true;
         }
-        if(Input.GetKeyDown(KeyCode.Alpha1)){
+
+        if (Input.GetKeyDown("s")) {
+            stopBall();
+        }
+        if (Input.GetKeyDown("r")) {
+            stopBall();
+            resetBall();
+        }
+
+        /*if(Input.GetKeyDown(KeyCode.Alpha1)){
             power = 10;
         }
         if(Input.GetKeyDown(KeyCode.Alpha2)){
@@ -54,19 +93,10 @@ public class HitBallBehaviour : MonoBehaviour {
         }
         if(Input.GetKeyDown(KeyCode.Alpha6)){
             power = 60;
-        }
+        }*/
 
-        if (Input.GetKeyDown("s")) {
-            stopBall();
-        }
-
-        if (Input.GetKeyDown("r")) {
-            stopBall();
-            resetBall();
-        }
-
-		// Hide arrow when ball is in motion
-		if (rb.velocity == Vector3.zero) {
+        // Hide arrow when ball is in motion
+        if (rb.velocity == Vector3.zero) {
 			foreach (Renderer rend in arrowRend) {
 				rend.enabled = true;
 			}
@@ -76,11 +106,6 @@ public class HitBallBehaviour : MonoBehaviour {
 				rend.enabled = false;
 			}
 		}
-
-        //Power meter stuff
-        //while (Input.GetMouseButtonDown(0)) {
-        //    Mathf.Sin(2);
-        //}
     }
 
     public static void stopBall() {
@@ -90,5 +115,42 @@ public class HitBallBehaviour : MonoBehaviour {
 
     public static void resetBall() {
         rb.transform.position = startPos;
+    }
+
+    private void checkStage()
+    {
+        switch (stage)
+        {
+            case 0:
+                windSpd[0] = Stage0.windSpdMin;
+                windSpd[1] = Stage0.windSpdMax;
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            case 11:
+                break;
+            case 12:
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ GH/GolfHero/Assets/Scripts/Stage0.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage0 : MonoBehaviour {
+    public static int windSpdMax = 5;
+    public static int windSpdMin = 20;
+}
