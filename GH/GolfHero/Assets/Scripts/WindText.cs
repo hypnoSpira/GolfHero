51a163bf372b7aae929a1a50e81f658ec6129bd8
Bryan Delgado-Bosso <bryan.delgado.bosso@gmail.com>
2017-07-03 14:23:13 -0400

    System Design Added
    Turned it into a PDF and created a proper cover page. Additional CRC
    cards added as well.

---
+++ GH/GolfHero/Assets/Scripts/WindText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WindText : MonoBehaviour {
+
+    private static Text txt;
+
+	// Use this for initialization
+	void Start () {
+        txt = GetComponent(typeof(Text)) as Text;
+	}
+	
+    public static void setText(string windTxt) {
+        txt.text = windTxt;
+    }
+}
