6fb3bdf849ee720f67699a8d127e70b57aa1db84
John Fraser <johnfras@gmail.com>
2017-07-03 22:20:14 -0400

    [#148270429] Fixed escaping the stage
    Added script for skybox where exiting calls reset ball

---
+++ GH/GolfHero/Assets/BoundaryBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	void OnTriggerExit(Collider other) {
+        if (other.gameObject.Equals(GameObject.Find("Ball"))){
+			HitBallBehaviour.resetBall();
+			Debug.Log("Ball has gone out of bounds");
+		}
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
