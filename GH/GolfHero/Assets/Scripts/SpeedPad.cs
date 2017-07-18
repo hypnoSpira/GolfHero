17b72114924144e969e80c50dbe8f7705c89201c
Bryan Delgado-Bosso <bryan.delgado.bosso@gmail.com>
2017-07-17 21:36:45 -0400

    Fixed Level 3 Boostpad for Main

--- GH/GolfHero/Assets/Scripts/SpeedPad.cs
+++ GH/GolfHero/Assets/Scripts/SpeedPad.cs
@@ -6,23 +6,27 @@ public class SpeedPad : MonoBehaviour {
 	public float multiplier;
 	public float addition;
 	private Renderer rend;
+	private Rigidbody ball;
 
 	// Use this for initialization
 	void Start () {
 		rend = this.gameObject.GetComponent<Renderer> ();
 	}
 	void OnTriggerEnter(Collider other) {
-        if (other.gameObject.Equals(GameObject.Find("Ball"))){
-			HitBallBehaviour.boostCurrentForce(multiplier, addition);
-			Debug.Log("Ball entered");
+		if (other.tag == "Ball"){
+			ball = other.GetComponent<Rigidbody> ();
+			Vector3 normalizedVelocity = Vector3.Normalize(ball.velocity);//Get the current velocity
+			float prevMag = ball.velocity.magnitude;//Use for multiplier
+			ball.AddForce(normalizedVelocity * (addition));//Perform addition first then multiply
+			ball.AddForce(normalizedVelocity * ((multiplier -1f) * prevMag));
 		}
     }
 	
 	// Update is called once per frame
 	void Update () {
+		// Animate texture for speed boosting appeal
 		Vector2 temp = rend.material.mainTextureOffset;
 		temp -= new Vector2 (0.0f, 0.007f);
-		Debug.Log (rend.material.mainTextureOffset);
 		rend.material.mainTextureOffset = temp;
 	}
 }
