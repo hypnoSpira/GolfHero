afedc730cca3895a426021aa0fd3e3dbf73523c8
Eugenia <eugenia.zhang@mail.utoronto.ca>
2017-07-22 21:15:49 -0400

    renaming PlayerController.cs to avoid conflict with Unity's nameing

--- GH/GolfHero/Assets/Scripts_Multiplayer/PlayerController_Original.cs
+++ GH/GolfHero/Assets/Scripts_Multiplayer/PlayerController_Original.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class PlayerController : NetworkBehaviour {
+public class PlayerController_Original : NetworkBehaviour {
 
     private CameraController cameraController;
 
