8d316b58276470fd38f07a4fd4aff570ea555faa
Eugenia <eugenia.zhang@mail.utoronto.ca>
2017-07-23 21:06:17 -0400

    Reverting commit afedc730cca3895a426021aa0fd3e3dbf73523c8 and fixing duplicate naming another way

--- GH/GolfHero/Assets/Scripts_Multiplayer/PlayerController.cs
+++ GH/GolfHero/Assets/Scripts_Multiplayer/PlayerController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class PlayerController_Original : NetworkBehaviour {
+public class PlayerController : NetworkBehaviour {
 
     private CameraController cameraController;
 
