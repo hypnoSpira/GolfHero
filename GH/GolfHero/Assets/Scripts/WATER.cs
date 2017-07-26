443b49d23331605e56a1e033a3ea81ab7ad2e238
Bryan Delgado-Bosso <bryan.delgado.bosso@gmail.com>
2017-07-25 20:46:42 -0400

    WATER

---
+++ GH/GolfHero/Assets/Scripts/WATER.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WATER : MonoBehaviour {
+
+	private Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = this.gameObject.GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Animate texture for MAXIMUM WATER MOVEMENT
+		Vector2 temp = rend.material.mainTextureOffset;
+		temp -= new Vector2 (0.0f, 0.007f);
+		rend.material.mainTextureOffset = temp;
+	}
+}
